e59106209d3f1a1833ab9850de6dedd892fab2cd
zveg <zveg90@gmail.com>
2015-01-25 21:35:11 +0200

    Added function to get hash for file comparison

--- my_script.rb
+++ my_script.rb
@@ -2,3 +2,11 @@ require 'flickraw'
 
 FlickRaw.api_key=ENV['FLICK_API_KEY']
 FlickRaw.shared_secret=ENV['FLICK_SECRET']
+
+flickr.access_token = ENV['TOKEN']
+flickr.access_secret = ENV['SECRET']
+
+
+def get_hash(path)
+  Digest::MD5.file(path).hexdigest
+end
