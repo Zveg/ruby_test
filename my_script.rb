72247f412148f5e2f2ff90440a881f5c2a890666
zveg <zveg90@gmail.com>
2015-01-26 22:06:56 +0200

    Refactored a bit

--- my_script.rb
+++ my_script.rb
@@ -1,25 +1,34 @@
 require 'flickraw'
-puts "test"
-FlickRaw.api_key=ENV['FLICK_API_KEY']
-FlickRaw.shared_secret=ENV['FLICK_SECRET']
-puts "test"
-flickr.access_token = ENV['TOKEN']
-
-puts flickr.access_token
-
-flickr.access_secret = ENV['SECRET']
+=begin
 
 login = flickr.test.login
 puts "You are now authenticated as #{login.username}"
+=end
 
 def get_hash(path)
   Digest::MD5.file(path).hexdigest
 end
 
-def upload(path)
-resp =  flickr.upload_photo path
-puts resp
+
+
+
+class ImageUploader
+  attr_accessor :flickr
+  def initialize
+      FlickRaw.api_key=ENV['FLICK_API_KEY']
+      FlickRaw.shared_secret=ENV['FLICK_SECRET']
+      flickr = FlickRaw::Flickr.new
+      flickr.access_token = ENV['TOKEN']
+      flickr.access_secret = ENV['SECRET']
+      @flickr = flickr
+  end
+
+  def upload(path)
+    flickr.upload_photo path
+  end
+
 end
 
+puts ImageUploader.new.upload('/home/zveg/1.jpg')
 
-upload(testPhoto)
+
